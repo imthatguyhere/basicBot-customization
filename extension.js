810ea775e6384cdd72b6f3de869e61048e94fac2
Tyler Morrison <imthatguyhere@gmail.com>
2015-01-28 17:55:04 +0000

    plug command added due to maintenance

--- extension.js
+++ extension.js
@@ -44,6 +44,7 @@
                 }
             }
         };
+        
         bot.commands.bodCommand = {
             command: 'bod',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
@@ -56,6 +57,20 @@
                 }
             }
         };
+        
+        bot.commands.plugCommand = {
+            command: 'plug',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me Plug.dj Your updates can't hold me down. Screw your maintenance! ;P Love you guys!");
+                }
+            }
+        };
+        
 
         //Load the chat package again to account for any changes
         bot.loadChat();
